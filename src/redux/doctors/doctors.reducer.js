c876faa70aa758d3d9cd421908b415faa974691a
JaviAzagra <javiazagra12@gmail.com>
2022-12-03 12:29:28 +0100

    FEAT: added cuadro medico and redux doctors

---
+++ src/redux/doctors/doctors.reducer.js
@@ -0,0 +1,31 @@
+const INITIAL_STATE = {
+    doctors: [],
+    isLoading: false,
+    error: false,
+  };
+  
+  const doctorsReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case "gettingDoctors":
+        return { ...state, isLoading: true };
+      case "getDoctors":
+        return {
+          ...state,
+          isLoading: false,
+          doctors: action.payload,
+          error: false,
+        };
+      case "errorDoctors":
+        return {
+          ...state,
+          isLoading: false,
+          doctors: [],
+          error: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default doctorsReducer;
