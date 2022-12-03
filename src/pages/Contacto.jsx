3d6ed51b45949a1c67f174946076f4b2a3284355
JaviAzagra <javiazagra12@gmail.com>
2022-12-03 23:34:10 +0100

    FEAT: added in contact page a title and added styles

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import FormContact from '../componentes/FormContact';
-import Map from '../componentes/Map';
+import React from "react";
+import FormContact from "../componentes/FormContact";
+import Map from "../componentes/Map";
 import "../styles/Contacto.scss";
 
 const Contacto = () => {
   return (
-    <div className='contact'>
-    <FormContact/>
-    <div className='contact'>
-    <ul className="contact__ul">
-        <li className="contact__ul--li">
-          Tel: 900 302 013 / info@healthalgorithm.es
-        </li>
-      </ul>
+    <div className="contact">
+      <h2 className="contact__title">Dudas y Consultas</h2>
+      <div className="contact">
+        <ul className="contact__ul">
+          <li className="contact__ul--li">
+            Tel: 900 302 013 / info@healthalgorithm.es
+          </li>
+        </ul>
+      </div>
+      <FormContact />
+      <Map />
     </div>
-    <Map />
-    </div>
-  )
-}
+  );
+};
 
-export default Contacto
+export default Contacto;
