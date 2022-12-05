9045e12318c83dbd241fdea56391ad131431e7d1
carlotajuncosa <carlota.juncosa@gmail.com>
2022-12-05 10:56:47 +0100

    FEAT styles responsive  at authors page and FIX description in index.html

--- src/pages/Authors.jsx
+++ src/pages/Authors.jsx
@@ -7,7 +7,7 @@ const Authors = () => {
       <h2 className="authors__title title">Creators</h2>
       <p className="authors__p p">
         Health Algorithm is a project made by Neftalí de la Cruz, Javi Azagra,
-        Adolfo Tardón and Carlota Juncosa with React JS.
+        Adolfo Gómez and Carlota Juncosa with React JS.
       </p>
       {/* CONTAINER TARGETAS ROW */}
       <div className="authors__container">
@@ -23,8 +23,8 @@ const Authors = () => {
           <div className="authors__info">
             <h4>Neftalí De La Cruz </h4>
             <p className="authors__p">
-              <a href="https://github.com/NeftaliCruzSosa">GitHub</a> |{" "}
-              <a href="https://www.linkedin.com/in/neftali-de-la-cruz-sosa-7bb40b21a/">
+              <a href="https://github.com/NeftaliCruzSosa" rel="noreferrer" target="_blank">GitHub</a> |{" "}
+              <a href="https://www.linkedin.com/in/neftali-de-la-cruz-sosa-7bb40b21a/" rel="noreferrer" target="_blank">
                 Linkedin
               </a>
             </p>
@@ -43,8 +43,8 @@ const Authors = () => {
           <div className="authors__info">
             <h4>Javier Azagra</h4>
             <p className="authors__p">
-              <a href="https://github.com/JaviiAzagra">GitHub</a> |{" "}
-              <a href="https://www.linkedin.com/search/results/all/?heroEntityKey=urn%3Ali%3Afsd_profile%3AACoAADcOll8BJRj7yXxTHS11VU10uL94Jq6UGe8&keywords=javier%20azagra%20garc%C3%ADa&origin=RICH_QUERY_SUGGESTION&position=0&searchId=97292c35-65b2-43eb-b43f-38a72fe3b4df&sid=2Hp">
+              <a rel="noreferrer" target="_blank" href="https://github.com/JaviiAzagra">GitHub</a> |{" "}
+              <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/javier-azagra-garc%C3%ADa/">
                 Linkedin
               </a>
             </p>
@@ -63,8 +63,8 @@ const Authors = () => {
           <div className="authors__info">
             <h4>Adolfo Gómez</h4>
             <p className="authors__p">
-              <a href="https://github.com/DarkoGL">GitHub</a> |{" "}
-              <a href="https://www.linkedin.com/in/adolfo-g%C3%B3mez-ligero-bb01b3244/">
+              <a rel="noreferrer" target="_blank" href="https://github.com/DarkoGL">GitHub</a> |{" "}
+              <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/adolfo-g%C3%B3mez-ligero-bb01b3244/">
                 Linkedin
               </a>
             </p>
@@ -83,8 +83,8 @@ const Authors = () => {
           <div className="authors__info">
             <h4>Carlota Juncosa</h4>
             <p className="authors__p">
-              <a href="https://github.com/carlotajuncosa">GitHub</a> |{" "}
-              <a href="/">Linkedin</a>
+              <a rel="noreferrer" target="_blank" href="https://github.com/carlotajuncosa">GitHub</a> |{" "}
+              <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/carlota-juncosa-b33604119/">Linkedin</a>
             </p>
           </div>
         </div>
